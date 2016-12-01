2e55201f82854d69b40d17f23f0cd099921d6100
Luca Tamburo <l.tamburo.shiftleft@gmail.com>
2016-12-01 15:37:11 +0000

    update dist files

--- js/angular-daterangepicker.js
+++ js/angular-daterangepicker.js
@@ -11,7 +11,7 @@
     }
   });
 
-  picker.directive('dateRangePicker', function($compile, $timeout, $parse, dateRangePickerConfig) {
+  picker.directive('dateRangePicker', ['$compile', '$timeout', '$parse', 'dateRangePickerConfig', function($compile, $timeout, $parse, dateRangePickerConfig) {
     return {
       require: 'ngModel',
       restrict: 'A',
@@ -227,6 +227,6 @@
         });
       }
     };
-  });
+  }]);
 
 }).call(this);
